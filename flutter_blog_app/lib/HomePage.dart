4e0b93f8d85e75a23d2d34f992d1a244f6cd3be6
KandabadaSenarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-01-24 14:59:39 +0530

    Home Page UI

---
+++ flutter_blog_app/lib/HomePage.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget
+{
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage>
+{
+
+  void _logoutUser()
+  {
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      appBar: new AppBar(
+
+        title: new Text("Home"),
+      ),
+
+      body: new Container(
+
+      ),
+
+      bottomNavigationBar: new BottomAppBar(
+
+        color: Colors.pink,
+
+        child: new Container(
+
+          margin: const EdgeInsets.only(left: 70.0, right: 70.0),
+
+          child: new Row(
+
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+
+            children: <Widget>[
+
+              new IconButton(
+
+                icon: new Icon(Icons.local_car_wash),
+                iconSize: 50,
+                color: Colors.white,
+
+                onPressed: _logoutUser,
+              ),
+
+              new IconButton(
+
+                icon: new Icon(Icons.add_a_photo),
+                iconSize: 40,
+                color: Colors.white,
+
+                onPressed: null,
+              )
+            ],
+
+          ),
+
+        ),
+
+      ),
+
+    );
+  }
+}
--- flutter_blog_app/lib/main.dart
+++ flutter_blog_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'LoginRegisterPage.dart';
+import 'HomePage.dart';
 
 void main()
 {
@@ -17,7 +18,7 @@ class BlogApp extends StatelessWidget
         primarySwatch: Colors.pink,
       ),
 
-      home: LoginRegisterPage(),
+      home: HomePage(),
 
     );
   }
